10f66e2701ee4e21e9c8d92d24dd0f7209855a25
JongHyeon Choi <chn9801@gmail.com>
2023-01-16 16:11:23 +0900

    Feat(FE):add Naver-Login 카카오 로그인 연결 완료 Related to:#3

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -1,5 +1,6 @@
 import { KAKAO_AUTH_URL } from "./KakaoLoginData";
 import NaverLogin from "./NaverLogin"
+import KakaoLogin from "./KakaoLogin"
 import React from "react";
 
 const Login = () => {
@@ -8,10 +9,7 @@ const Login = () => {
 	return (
 		<div>
 			<p>회원가입 창</p>
-			<button href={KAKAO_AUTH_URL}>
-				<img src="../images/kakao_login_medium_wide.png" alt="" />
-				<span>카카오계정 로그인</span>
-			</button>
+			<KakaoLogin />
 			<NaverLogin />
 		</div>
 	)

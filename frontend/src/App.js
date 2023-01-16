c3a0035a4697dcbec69fa8e5f53a3de2f4d5684b
JongHyeon Choi <chn9801@gmail.com>
2023-01-16 13:56:16 +0900

    Feat(FE):add components structure 컴포넌트 구조에 따라 라우트 구축 Related to:
    .exit

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Router/Main'
+import Register from './Router/Register'
+import Edit from './Router/Edit'
+import Profile from './Router/Profile'
+import Detail from './Router/Detail'
+import Search from './Router/Search'
+import Live from './Router/Live'
+import Chat from './Router/Chat'
+// import Login from './Router/Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Router>
+        <Routes>
+
+          <Route path="/" element={
+            <Main />
+          } />
+
+          <Route path="/register" element={
+            <Register />
+          } />
+          
+          <Route path="/edit" element={
+            <Edit />
+          } />
+
+          <Route path="/chat" element={
+            <Chat />
+          } />
+
+          <Route path="/live" element={
+            <Live />
+          } />
+
+          <Route path="/profile" element={
+            <Profile />
+          } />
+
+          <Route path="/detail" element={
+            <Detail />
+          } />
+
+          <Route path="/search" element={
+            <Search />
+          } />
+
+        </Routes>
+      </Router>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Router/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+
+  return (
+    <div>
+      <p>메인페이지입니다</p> 
+    </div>
+  )
+}
+
+export default Main

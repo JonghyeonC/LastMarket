3d1cbef0171058210f9d8eeabc78f28bf66e4010
rnjswngus <rnjswngus275@gmail.com>
2023-01-16 23:22:58 +0900

    Feat(M): add convert-address
    위도경도를 동으로 바꾸는 코드 추가
    
    Related to:#3

--- mobile/LastMarket/app/src/main/java/com/jphr/lastmarket/activity/UserInfoActivity.kt
+++ mobile/LastMarket/app/src/main/java/com/jphr/lastmarket/activity/UserInfoActivity.kt
@@ -1,14 +1,20 @@
 package com.jphr.lastmarket.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
-import androidx.activity.viewModels
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.jphr.lastmarket.databinding.ActivityUserInfoBinding
 import com.jphr.lastmarket.service.UserInfoService
+import java.io.IOException
+import java.util.*
+
 
 private const val TAG = "UserInfoActivity"
 class UserInfoActivity : AppCompatActivity() {
@@ -38,15 +44,29 @@ class UserInfoActivity : AppCompatActivity() {
         })
 
 
-
-
-
         binding.save.setOnClickListener{
             userName= binding.userName.text.toString()
             userJob= binding.userJob.selectedItem as String
             userCategory=binding.userCategory.selectedItem as String
         }
 
+    }
+    fun getAddress(mContext: Context?, lat: Double, lng: Double): String? {//지오코더를 사용하여 위경도를 주소로 변환
+        var nowAddr = "현재 위치를 확인 할 수 없습니다."
+        val geocoder = Geocoder(mContext, Locale.KOREA)
+        val address: List<Address>?
+        try {
+            if (geocoder != null) {
+                address = geocoder.getFromLocation(lat, lng, 1)
+                if (address != null && address.isNotEmpty()) {
+                    nowAddr = address[0].getAddressLine(0).toString()
+                }
+            }
+        } catch (e: IOException) {
+            Toast.makeText(mContext, "주소를 가져 올 수 없습니다.", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
+        return nowAddr
     }
 
 }

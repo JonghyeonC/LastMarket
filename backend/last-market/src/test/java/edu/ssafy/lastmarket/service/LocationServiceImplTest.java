8b33870382af5d46fcffb213bbeece54a9ea8a72
limecats <limecats0331@gmail.com>
2023-01-18 15:46:20 +0900

    feat(BE): add find by address
    문자열 주소로 동코드 검색기능 추가
    테스트 추가

---
+++ backend/last-market/src/test/java/edu/ssafy/lastmarket/service/LocationServiceImplTest.java
@@ -0,0 +1,58 @@
+package edu.ssafy.lastmarket.service;
+
+import edu.ssafy.lastmarket.domain.eneity.Location;
+import edu.ssafy.lastmarket.repository.LocationRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class LocationServiceImplTest {
+
+    @Mock
+    LocationRepository locationRepository;
+
+    LocationServiceImpl locationService;
+
+
+    @Test
+    @DisplayName("지역명으로 동코드 찾기")
+    public void locationExist() {
+        mocking();
+        //given
+        String address = "서울시 용산구 도원동";
+        //when
+        String dongCode = locationService.findDongCodeByAddress(address);
+        //then
+        assertThat(dongCode).isEqualTo("1117012000");
+    }
+
+    @Test
+    @DisplayName("지역명이 4개로 나눠짐")
+    public void locationSplit4() {
+        locationService = new LocationServiceImpl(locationRepository);
+        //given
+        String address = "서울시 용산구 무슨동 3가";
+        //when
+        assertThatThrownBy(() -> locationService.findDongCodeByAddress(address))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    public void mocking() {
+        Location location = Location.builder()
+                .sido("서울시")
+                .gugun("용산구")
+                .dong("도원동")
+                .dongCode("1117012000")
+                .build();
+        doReturn(Optional.of(location)).when(locationRepository).findBySidoAndDongAndGugun("서울시", "용산구", "도원동");
+        locationService = new LocationServiceImpl(locationRepository);
+    }
+}

057a74ced1ee2ea95f365813022e45eef6e94f08
qgw13467 <qgw13467@naver.com>
2023-01-20 14:36:01 +0900

    feat(BE): add member_profile
    유저 프로파일 등록기능 추가

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/controller/MemberController.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/controller/MemberController.java
@@ -1,16 +1,20 @@
 package edu.ssafy.lastmarket.controller;
 
 import edu.ssafy.lastmarket.domain.dto.MemberRegistDto;
+import edu.ssafy.lastmarket.service.ImageUploadService;
 import edu.ssafy.lastmarket.service.MemberCategoryService;
 import edu.ssafy.lastmarket.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
-
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -18,6 +22,7 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberCategoryService memberCategoryService;
 
+    private final ImageUploadService imageUploadService;
     @PostMapping("/user")
     public ResponseEntity<?> registMember(Principal principal, MemberRegistDto memberRegistDto){
 
@@ -26,4 +31,13 @@ public class MemberController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PatchMapping("/user/profile")
+    public ResponseEntity<?> registProfile(Principal principal,  @RequestParam("image")MultipartFile multipartFile) throws IOException {
+
+        memberService.updateProfile(imageUploadService.upload(multipartFile),principal.getName());
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }

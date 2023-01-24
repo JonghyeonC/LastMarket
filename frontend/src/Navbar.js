cfc2732dd29859486096edce27bc62c870cca95c
Jonghyeon Choi <chn9801@gmail.com>
2023-01-25 00:53:02 +0900

    fix

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -20,20 +20,20 @@ function Navbar() {
         <img className="W_logo" src="logos/W_logo.png" alt="W_logo" onClick={() => navigate('/')}/>
         <span><input className="nav_input" type="text" placeholder="검색해보세요" onChange={(e) => setInputValue(e.target.value)} onKeyPress={(e) => { if (e.key === 'Enter') { navigate("/search/" + inputValue) } }} /></span>
         <span>
-          {/* <span>
+          <span>
             <div className="nav_btn_box"> 
               <img className="chat_icon" src="chat_icon.png" alt="chat_icon" onClick={() => navigate('/chat')}/>
               <img className="profile_icon" src="profile_icon.png" alt="profile_icon" onClick={() => navigate('/profile')}/>
               <img className="logout_icon" src="logout_icon.png" alt="logout_icon" onClick={() => navigate('/login')}/>
             </div>
-          </span> */}
+          </span>
           {/* <br /> */}
-          <span>
+          {/* <span>
             <div className="nav_btn_box">
               <img className="login_icon" src="login_icon.png" alt="login_icon" onClick={() => navigate('/login')} />
               <img className="signup_icon" src="signup_icon.png" alt="signup_icon" onClick={() => navigate('/signup')} />
             </div>
-          </span>
+          </span> */}
           {/* <br />
           <span>
             <div class="nav_btn_box">

46deed0c744f3f49887da13edb85d0c0dd591fed
qgw13467 <qgw13467@naver.com>
2023-01-25 17:14:39 +0900

    feat(BE): add product repository
    add product repository

---
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package edu.ssafy.lastmarket.repository;
+
+import edu.ssafy.lastmarket.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}

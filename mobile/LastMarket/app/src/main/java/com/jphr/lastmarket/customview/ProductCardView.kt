6226836eb8ba69297cd963f904da2c6fbfb25013
DESKTOP-DOGVPUB\SSAFY <rnjswngus275@gmail.com>
2023-01-25 09:10:45 +0900

    Refactor(M): refactor custom view
    프로덕트 리스트 커스텀 뷰 수정
    
    Related to:#7

--- mobile/LastMarket/app/src/main/java/com/jphr/lastmarket/customview/ProductCardView.kt
+++ mobile/LastMarket/app/src/main/java/com/jphr/lastmarket/customview/ProductCardView.kt
@@ -7,30 +7,22 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.card.MaterialCardView
 import com.jphr.lastmarket.R
 
 class ProductCardView (context: Context, attrs: AttributeSet): LinearLayout(context,attrs) {
-    //커스텀 뷰 안에 들어가는 아이템들
-//     var layout : MaterialCardView
+
      var image : ImageView
      var title: TextView
-     var secondText : TextView
-//     var supportText: TextView
-//     var button1:MaterialButton
-//     var button2:MaterialButton
+     var price : TextView
+
 
     init {
         val v = View.inflate(context, R.layout.product_card, this)
 
-//        layout = findViewById(R.id.card)
         image =findViewById(R.id.iv)
         title=findViewById(R.id.title)
-        secondText=findViewById(R.id.second)
-//        supportText=findViewById(R.id.support)
-//        button1=findViewById(R.id.button1)
-//        button2=findViewById(R.id.button2)
+        price=findViewById(R.id.price)
+
 
         context.theme?.obtainStyledAttributes(
             attrs,
@@ -38,11 +30,8 @@ class ProductCardView (context: Context, attrs: AttributeSet): LinearLayout(cont
             0, 0)?.apply {
             try {
                 setImage(getString(R.styleable.CardView_image))
-                setTitleText(getString(R.styleable.CardView_title))
-                setSecondText(getString(R.styleable.CardView_second_text))
-                setSupportText(getString(R.styleable.CardView_second_text))
-//                setButtonText(getString(R.styleable.CardView_button_text1))
-//                setButton2Text(getString(R.styleable.CardView_button_text2))
+                setTitle(getString(R.styleable.CardView_title))
+                setPrice(getString(R.styleable.CardView_price))
 
             } finally {
                 recycle()
@@ -54,28 +43,15 @@ class ProductCardView (context: Context, attrs: AttributeSet): LinearLayout(cont
     fun setImage(url: String?){
         Glide.with(context).load(url).into(image)
     }
-    fun setTitleText(text: String?){
+    fun setTitle(text: String?){
         title.text = text
         onRefresh()
     }
 
-    fun setSecondText(text: String?){
-        secondText.text = text
+    fun setPrice(text: String?){
+        price.text = text
         onRefresh()
     }
-
-    fun setSupportText(text: String?){
-        secondText.text = text
-        onRefresh()
-    }
-//    fun setButtonText(text: String?){
-//        button1.text = text
-//        onRefresh()
-//    }
-//    fun setButton2Text(text: String?){
-//        button2.text = text
-//        onRefresh()
-//    }
 
     private fun onRefresh(){
         invalidate()

7270c3409c29d87729f1545d72e209d21ea8c36b
박희종 <qgw13467@naver.com>
2023-01-25 20:25:21 +0900

    fix(BE): modify save method
    상품의 위치를 사용자의 위치로 저장

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/service/ProdoctServiceImpl.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/service/ProdoctServiceImpl.java
@@ -2,8 +2,11 @@ package edu.ssafy.lastmarket.service;
 
 import edu.ssafy.lastmarket.domain.dto.ProductDto;
 import edu.ssafy.lastmarket.domain.dto.ProductListDto;
+import edu.ssafy.lastmarket.domain.entity.Image;
 import edu.ssafy.lastmarket.domain.entity.Member;
 import edu.ssafy.lastmarket.domain.entity.Product;
+import edu.ssafy.lastmarket.domain.entity.ProductImage;
+import edu.ssafy.lastmarket.repository.ProductImageRepository;
 import edu.ssafy.lastmarket.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +22,7 @@ import java.util.Optional;
 public class ProdoctServiceImpl implements ProductService{
 
     private final ProductRepository productRepository;
+    private final ProductImageRepository productImageRepository;
     @Override
     public Page<ProductListDto> getProducts(Pageable pageable) {
         return null;
@@ -27,14 +32,24 @@ public class ProdoctServiceImpl implements ProductService{
     public Product save(ProductDto productDto, Member member) {
         Product product = ProductDto.convert(productDto);
         product.setSeller(member);
+        product.setLocation(member.getLocation());
 
         return productRepository.save(product);
 
     }
 
     @Override
-    public Optional<Product> saveImgs(Product product, MultipartFile[] multipartFiles, Member member) {
-        return Optional.empty();
+    public Product saveImgs(Product product, List<Image> images, Member member) {
+        List<ProductImage> productImages = product.getImages();
+        for (Image image : images) {
+            ProductImage productImage = new ProductImage(product, image);
+            ProductImage save = productImageRepository.save(productImage);
+            productImages.add(save);
+
+        }
+
+
+        return product;
     }
 
     @Override

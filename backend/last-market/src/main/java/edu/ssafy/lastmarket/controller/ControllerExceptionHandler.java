be46122a3bfafdbcd92f61ba6be507cfd81a8c81
박희종 <qgw13467@naver.com>
2023-01-25 23:14:31 +0900

    feat(BE): add customexception
    커스텀 예외 추가

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/controller/ControllerExceptionHandler.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/controller/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package edu.ssafy.lastmarket.controller;
 
 import edu.ssafy.lastmarket.exception.BanExistException;
 import edu.ssafy.lastmarket.exception.NotMemberUsernameException;
+import edu.ssafy.lastmarket.exception.NotYourAuthority;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.support.ErrorMessage;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,6 +33,21 @@ public class ControllerExceptionHandler {
         result.put("error msg", e.toString());
         result.put("msg", "user is alreay banned");
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(NotYourAuthority.class)
+    public ResponseEntity<?> notYouAuthrity(Exception e) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("error msg", e.toString());
+        result.put("msg", "notYouAuthrity");
+        return new ResponseEntity<>(result, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<?> iOException(Exception e) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("error msg", e.toString());
+        result.put("msg", "iOException");
+        return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }

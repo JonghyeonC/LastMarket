242c4d1d2e4798b66ae27cc475a58a5c79cdae03
qgw13467 <qgw13467@naver.com>
2023-01-25 13:20:22 +0900

    fix(BE): modify SecurityConfig
    SecurityConfig 수정

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/security/SecurityConfig.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/security/SecurityConfig.java
@@ -3,14 +3,17 @@ package edu.ssafy.lastmarket.security;
 import edu.ssafy.lastmarket.service.PrincipalOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -21,19 +24,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        
+
         //임시로 처리
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/user").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-
+//                .antMatchers(HttpMethod.POST, "/user").authenticated()
+                .anyRequest().permitAll();
         http.exceptionHandling()
-                        .accessDeniedHandler((request, response, accessDeniedException) -> {
-                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                        });
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    }
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                });
+
+
+
         http.csrf().disable();
         http.headers()
                 .frameOptions()
@@ -43,7 +51,8 @@ public class SecurityConfig {
 //                .defaultSuccessUrl("/")
                 .userInfoEndpoint() // 필수
                 .userService(principalOauth2UserService)
-                .and().successHandler(oAuth2AuthenticationSuccessHandler)
+                .and().
+                successHandler(oAuth2AuthenticationSuccessHandler)
         ;
         return http.build();
     }

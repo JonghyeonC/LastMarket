839927f4292a23639be31c1314a9464bba15aa1d
limecats <limecats0331@gmail.com>
2023-01-25 12:54:44 +0900

    feat(BE): add argument resolver

---
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,33 @@
+package edu.ssafy.lastmarket.argumentresolver;
+
+import edu.ssafy.lastmarket.domain.eneity.Member;
+import edu.ssafy.lastmarket.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return memberRepository.findByUsername(principal).get();
+    }
+}

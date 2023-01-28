2de64a20845aec2c84ce58bd5d8ea235dd9507a6
박희종 <qgw13467@naver.com>
2023-01-28 12:58:00 +0900

    feat(BE): add check favorite
    찜 여부 확인

---
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/repository/FavoriteRepository.java
@@ -0,0 +1,16 @@
+package edu.ssafy.lastmarket.repository;
+
+import edu.ssafy.lastmarket.domain.entity.Favorite;
+import edu.ssafy.lastmarket.domain.entity.Member;
+import edu.ssafy.lastmarket.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    Optional<Favorite> findByMemberAndProductId(Member member, Long ProductId);
+    Boolean existsByMemberAndProductId(Member member, Long ProductId);
+
+}

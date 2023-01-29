6dd6cf0c810fbd950a8be709e0939dec3ad06f94
Jonghyeon Choi <chn9801@gmail.com>
2023-01-30 01:05:04 +0900

    Feat(FE):add-profile page 유저 프로필 페이지 구성 추가

--- frontend/src/Router/Main.js
+++ frontend/src/Router/Main.js
@@ -1,31 +1,7 @@
 import Card from "../Components/Card"
+import products from "../Data"
 
 function Main() {
-  const products = [
-    {
-      id : 0,
-      title : "White and Black",
-      price : 120000
-    },
-  
-    {
-      id : 1,
-      title : "Red Knit",
-      price : 110000
-    },
-  
-    {
-      id : 2,
-      title : "Grey Yordan",
-      price : 130000
-    },
-
-    {
-      id : 3,
-      title : "Check Snikers",
-      price : 90000
-    },
-  ]
 
   return (
     <div>

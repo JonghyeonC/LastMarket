3c11fb1d3656788525392d9d1f7a86e43e2d4afd
YuhongKim <loyyir1223@naver.com>
2023-01-30 10:15:34 +0900

    modified and merge

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,6 +1,54 @@
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Category from "./Category";
+
+const options = [
+  {
+    name: 'Enable backdrop (default)',
+    scroll: false,
+    backdrop: true,
+  },
+];
+
+function OffCanvasExample({ name, ...props }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
+  return (
+    <>
+    <div>
+      <Button variant="primary" onClick={toggleShow} className="me-2">
+        <img className="category_btn1" src="category_btn.png" alt="category_btn1"/>
+      </Button>
+      <Offcanvas className="cate_zone" show={show} onHide={handleClose} {...props}>
+        {/* <Offcanvas.Header closeButton> */}
+          <Offcanvas.Title><h2>지금 뭐해요??</h2></Offcanvas.Title>
+        {/* </Offcanvas.Header> */}
+        <hr />
+        <Category />
+      </Offcanvas>
+    </div>
+    </> 
+  );
+}
+
+function Example() {
+  return (
+    <>
+      {options.map((props, idx) => (
+        <OffCanvasExample key={idx} {...props} />
+      ))}
+    </>
+  );
+}
+
+
+
 function Navbar() {
   
   let navigate = useNavigate()
@@ -16,7 +64,8 @@ function Navbar() {
         </div>
         <a href="https://www.google.com">고객센터</a>
       </div>
-      <hr className="nav_line1"/>
+      {/* <hr className="nav_line1"/> */}
+      <br />
       <div className="navbar_down">
         <img className="W_logo" src="logos/W_logo.png" alt="W_logo" onClick={() => navigate('/')}/>
         <span><input className="nav_input" type="text" placeholder="검색해보세요" onChange={(e) => setInputValue(e.target.value)} onKeyPress={(e) => { if (e.key === 'Enter') { navigate("/search/" + inputValue) } }} /></span>
@@ -24,7 +73,7 @@ function Navbar() {
           <span>
             <div className="nav_btn_box"> 
               <img className="chat_icon" src="chat_icon.png" alt="chat_icon" onClick={() => navigate('/chat')}/>
-              <img className="profile_icon" src="profile_icon.png" alt="profile_icon" onClick={() => navigate('/profile')}/>
+              <img className="myprofile_icon" src="myprofile_icon.png" alt="myprofile_icon" onClick={() => navigate('/profile')}/>
               <img className="logout_icon" src="logout_icon.png" alt="logout_icon" onClick={() => navigate('/login')}/>
             </div>
           </span>
@@ -44,7 +93,20 @@ function Navbar() {
           </span> */}
         </span>
       </div>
-      <hr className="nav_line2"/>
+      <div>
+        <span>
+          <Example />
+      
+          {/* // onClick={() => this.setState({ isCategory: !this.state.isCategory })} */}
+          {/* // />
+          // <Left primary={this.state.isCategory}>
+          //   <Cate>
+          //   <Category />
+          //   </Cate>
+          // </Left> */}
+          <hr className="nav_line2"/>
+        </span>
+      </div>
     </div>
   )
 }

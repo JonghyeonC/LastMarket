e0453c5d71047dff8eed23c148074b7a14fcb808
limecats <limecats0331@gmail.com>
2023-01-31 13:37:19 +0900

    feat(BE): add logInterceptor

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebConfig.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebConfig.java
@@ -1,9 +1,11 @@
 package edu.ssafy.lastmarket.config;
 
 import edu.ssafy.lastmarket.argumentresolver.LoginMemberArgumentResolver;
+import edu.ssafy.lastmarket.interceptor.LogInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,9 +15,17 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final LogInterceptor logInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .order(1)
+                .addPathPatterns("/**");
     }
 }
---
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/interceptor/LogInterceptor.java
@@ -0,0 +1,28 @@
+package edu.ssafy.lastmarket.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("[{}] connection ", request.getRequestURI());
+        Cookie authentication = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals("Authentication"))
+                .findFirst()
+                .orElse(null);
+        if (authentication != null) {
+            log.info("[{}] cookie={}", request.getRequestURI(), authentication.getValue());
+        }
+        return true;
+    }
+}

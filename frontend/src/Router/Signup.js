c0222ae576bbca66ebb70947713c989ce7154b6b
JongHyeon Choi <chn9801@gmail.com>
2023-01-31 12:36:26 +0900

    Feat(FE):add-location 유저 위치 출력 완료

--- frontend/src/Router/Signup.js
+++ frontend/src/Router/Signup.js
@@ -1,9 +1,7 @@
 import "./Signup.css"
 import { FaAngleDown } from "react-icons/fa"
-import { useEffect, useMemo, useState } from "react"
-import useCurrentLocation from '../Hooks/useCurrentPosition'
+import { useEffect, useState } from "react"
 import axios from "axios"
-import Location from "../Components/Location";
 
 function Signup() {
   
@@ -29,37 +27,48 @@ function Signup() {
     )
   })
   
-  const geolocationOptions = {
+  const options = {
     enableHighAccuracy: true,
-    timeout: 1000 * 60 * 1, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
-    maximumAge: 1000 * 3600 * 24, // 24 hour
-  }
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(position) {
+    //좌표를 알아낼 수 있는데, 여기서 알아낸 좌표를 kakaoAPI url에 사용할 것이다.
+    console.log('위도 : ' + position.coords.latitude); 
+    console.log('경도: ' + position.coords.longitude);
+  };
+  
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
 
-  const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
+  function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    
+    //kakao REST API에 get 요청을 보낸다.
+    //파라미터 x,y에 lon,lat을 넣어주고 API_KEY를 Authorization헤더에 넣어준다.
+    axios.get(`https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}&input_coord=WGS84`
+    ,{headers:{Authorization:`KakaoAK fb48a90139fb5e563023ad03ca49e216`}}
+    )
+    .then(res=>{
+        setLocation(res.data.documents[0].address.address_name)
+        dispatch(changeRegion(res.data.documents[0].address.region_1depth_name))
+        dispatch(changeCity(res.data.documents[0].address.region_2depth_name)) 
+    }
+    ).catch(e=>console.log(e))
+    }
+    function onGeoError(){
+        alert("위치권한을 확인해주세요");
+    }
 
-  useEffect(() => {
-    useCurrentLocation
-  }, [location]);
-
-  // const getAddr = ((location) => {
-  //   // 주소-좌표 변환 객체를 생성합니다
-  //   let geocoder = new kakao.maps.services.Geocoder();
-
-  //   let coord = new kakao.maps.LatLng(location.latitude, location.longitude);
-  //   let callback = function(result, status) {
-  //       if (status === kakao.maps.services.Status.OK) {
-  //           const arr  ={ ...result};
-  //           const _arr = arr[0].address.region_2depth_name;
-  //           console.log(_arr);
-  //       }
-  //   }
-  //   geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  // })
-
-  // useEffect(()=>{
-  //     getAddr(currentLocation);
-  // })
-
+    location = location.split(' ')
+    console.log(location)
+    //navigator.geolocation.getCurrentPosition(위치받는함수, 에러났을때 함수)
+    navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError)
 
   return (
     <div className='Signup'>
@@ -73,19 +82,9 @@ function Signup() {
       <div className='nameWrap'>
         <p className="labelBox">회원님은 어디에 있나요?</p>
         <div className='signupbox'>
-          <Location location={currentLocation} error={currentError} />
-          {/* <input id="dropdown1" type="checkbox" />
-          <label className='dropdownLabel1' for="dropdown1">
-            <div>어떤 일을 하는지 선택해주세요</div>
-            <FaAngleDown className='caretIcon' />
-          </label>
-          <div className='content'>
-            <ul>
-              <li onClick={() => {setJob(1)}}>1</li>
-              <li onClick={() => {setJob(2)}}>2</li>
-              <li onClick={() => {setJob(3)}}>3</li>
-            </ul>
-          </div> */}
+          <div>{location[0]}</div>
+          <div>{location[1]}</div>
+          <div>{location[2]}</div>
         </div>
       </div>
       <br />

e801b6a50e44e698f7bc3c68e3c7b60f93d7d553
limecats <limecats0331@gmail.com>
2023-02-02 14:27:22 +0900

    feat(BE): add ChatDTO

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/document/TradeChat.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/document/TradeChat.java
@@ -1,8 +1,6 @@
 package edu.ssafy.lastmarket.domain.document;
 
 import edu.ssafy.lastmarket.domain.dto.TradeChatDTO;
-import edu.ssafy.lastmarket.domain.entity.Member;
-import edu.ssafy.lastmarket.domain.entity.Product;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,15 +8,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.ArrayList;
 import java.util.List;
 
-@Document
+@Document(collection = "lastmarket")
 @Getter
 public class TradeChat {
     @Id
     private String id;
     private List<TradeChatDTO> chatLogs;
 
-    public TradeChat(Member seller, Member buyer, Product product, List<TradeChatDTO> chatLogs) {
-        this.id = seller.getUsername() + buyer.getUsername() + product.getId();
+    public TradeChat(String id) {
+        this.id = id;
         this.chatLogs = new ArrayList<>();
     }
 }
--- backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/dto/TradeChatDTO.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/dto/TradeChatDTO.java
@@ -1,7 +1,16 @@
 package edu.ssafy.lastmarket.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class TradeChatDTO {
     private String sender;
     private String msg;

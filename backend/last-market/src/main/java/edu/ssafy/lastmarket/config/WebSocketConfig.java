23f7e2c196630c21a8ca6393b127b553917a03d1
limecats <limecats0331@gmail.com>
2023-02-03 15:01:19 +0900

    feat(BE): add sock.js

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebSocketConfig.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebSocketConfig.java
@@ -26,7 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override

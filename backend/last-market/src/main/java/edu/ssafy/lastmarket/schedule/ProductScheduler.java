4f4e289407ee72c3e7432b606d40a720c2f574ed
qgw13467 <qgw13467@naver.com>
2023-02-03 17:30:17 +0900

    feat(BE): add product scheduler

---
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/schedule/ProductScheduler.java
@@ -0,0 +1,33 @@
+package edu.ssafy.lastmarket.schedule;
+
+import edu.ssafy.lastmarket.domain.entity.Product;
+import edu.ssafy.lastmarket.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductScheduler {
+
+    private final ProductService productService;
+
+    @Scheduled(cron = "* 10 * * * *")
+    public void productCheck(){
+        log.info("====now() : {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+        System.out.println(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+        List<Product> productByLivetime = productService.findProductByLivetime(LocalDateTime.now(ZoneId.of("Asia/Seoul")).minusMinutes(20));
+        for (Product product : productByLivetime) {
+
+            log.info("=====product: {} ", product.getId());
+        }
+        productService.changeDealstateToAfterbroadcast(productByLivetime);
+    }
+}

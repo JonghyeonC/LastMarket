d9a0a8a6d338180fe4285f5d40ad5e9d7f8f9404
YuhongKim <loyyir1223@naver.com>
2023-02-03 14:44:17 +0900

    Feat(FE) : GoodsList, Swiper, DetailpageLoad BE와 통신해서 상품 리스트 띄우기, 스와이퍼기능, 디테일 페이지 데이터 띄우기 추가 Related to : #

--- frontend/src/Pages/Detail/Discription.js
+++ frontend/src/Pages/Detail/Discription.js
@@ -1,11 +1,57 @@
+import { useEffect, useState } from 'react'
 import products from "../../Data"
 import './Discription.css'
+import axios from 'axios'
 
 
 function Discription(props) {
+  
+  const [ productDetail, setProductDetail ] = useState([])
+  const [ detailURIS, setDetailURIS ] = useState([])
+  function DiscriptionApi() {
+    
+    const url = `https://i8d206.p.ssafy.io/api/product/${props.id}`
+  
+    axios.get(url)
+    .then((res) => {
+      setProductDetail(res.data)
+      setDetailURIS(res.data.imgURIs)
+      // console.log(res.data.imgURIs)
+      console.log('success')
+      
+    })
+    .catch((res) => {
+      console.log('Failed')
+    })
+  }
+
+  useEffect(() => {
+    DiscriptionApi()
+  }, [])
+
+  console.log(productDetail)
+
   return (
     <div className="detailBox">
       <div>
+        {/* <img src={productDetail.imgURIs} width="500px" height="400px" alt="DetailImg" /> */}
+      </div>
+      <div>
+        <h1>{productDetail.title}</h1>
+        <div className="priceBox">
+          <p>{productDetail.startingPrice}</p>
+        </div>
+        <div>
+          {
+            detailURIS.map((imgURI) => {
+              return (
+                <img src={imgURI} width="500px" height="400px" alt="DetailImg" />
+              )
+            })
+          }
+        </div>
+      </div>
+      {/* <div>
         <img src={"https://codingapple1.github.io/shop/shoes" + (Number(props.id) + 1)+ ".jpg"} width="500px" height="400px" alt="" />
       </div>
       <div>
@@ -13,7 +59,7 @@ function Discription(props) {
         <div className="priceBox">
           <p>{products[props.id].price}</p>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }

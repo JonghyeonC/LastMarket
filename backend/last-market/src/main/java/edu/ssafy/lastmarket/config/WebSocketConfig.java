3445bcf69e4d904637d8125b82eceb01b9f20486
limecats <limecats0331@gmail.com>
2023-02-03 15:43:16 +0900

    fix(BE): without sockjs

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebSocketConfig.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/config/WebSocketConfig.java
@@ -26,10 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("*");
                 //.setAllowedOriginPatterns("http://localhost:3000")
-                .setAllowedOrigins("http://localhost:3000")
-                .withSockJS();
     }
 
     @Override

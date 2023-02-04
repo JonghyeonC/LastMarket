60e3b56d49c298e40db953f220870e73ba956122
박희종 <qgw13467@naver.com>
2023-02-04 19:27:48 +0900

    feat(BE): add livetime valid check

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/repository/ProductRepository.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package edu.ssafy.lastmarket.repository;
 
+import edu.ssafy.lastmarket.domain.entity.DealState;
 import edu.ssafy.lastmarket.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 @Repository

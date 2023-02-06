f93a4d6975722609f8acc08baf96e61f6c725b6b
qgw13467 <qgw13467@naver.com>
2023-02-06 10:25:39 +0900

    fix(BE): fix security config

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/security/SecurityConfig.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedMethods(
-                Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
+                Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         );
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -65,7 +65,7 @@ public class SecurityConfig {
         http.formLogin().disable();
         http.httpBasic().disable();
         http.csrf().disable();
-        http.cors().configurationSource(corsConfigurationSource());
+        http.cors();
         http.headers()
                 .frameOptions()
                 .sameOrigin();

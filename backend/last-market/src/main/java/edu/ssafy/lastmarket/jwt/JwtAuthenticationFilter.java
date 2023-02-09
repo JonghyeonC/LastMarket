55078ca44b651198d1bf7bdf101af407e87215cf
qgw13467 <qgw13467@naver.com>
2023-02-09 10:53:37 +0900

    feat(BE): add jwt no username redirect to signup

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/jwt/JwtAuthenticationFilter.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package edu.ssafy.lastmarket.jwt;
 
+import edu.ssafy.lastmarket.domain.entity.Member;
+import edu.ssafy.lastmarket.security.user.OAuth2UserImpl;
 import edu.ssafy.lastmarket.security.user.PrincipalOAuth2UserService;
 import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -30,10 +33,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-            String temp =jwtManager.parseJwt(request);
+            String token = jwtManager.parseJwt(request);
 
-            if(temp!=null&& jwtManager.isValidate(temp)){
-                String username = jwtManager.getUsername(temp);
+            if (token != null && jwtManager.isValidate(token)) {
+                String username = jwtManager.getUsername(token);
 
                 UserDetails userDetails = principalOAuth2UserService.loadUserByUsername(username);
                 Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
@@ -41,17 +44,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContext context = SecurityContextHolder.getContext();
                 context.setAuthentication(authentication);
                 SecurityContextHolder.setContext(context);
-            }
-        }catch (BadCredentialsException e){
-            loginFailure(request,response);
 
-        }
-        catch (Exception e) {
+                OAuth2UserImpl oAuth2User = (OAuth2UserImpl) userDetails;
+                Member member = oAuth2User.getMember();
+                if (StringUtil.isNullOrEmpty(member.getNickname())) {
+                    Cookie cookie = new Cookie("Authentication", token);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(3600);
+                    response.addCookie(cookie);
+                    response.addHeader("Authentication", token);
+                    response.setStatus(302);
+                    response.setHeader("Location", "/signup");
+                }
+
+            }
+        } catch (BadCredentialsException e) {
+            loginFailure(request, response);
+
+        } catch (Exception e) {
 
             e.printStackTrace();
         }
 
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 
 

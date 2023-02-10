543e2f12d80f68c00a2d34e83e242b207747de0a
YuhongKim <loyyir1223@naver.com>
2023-02-10 23:10:07 +0900

    restore

--- frontend/src/Pages/Register/InputPage.js
+++ frontend/src/Pages/Register/InputPage.js
@@ -28,7 +28,7 @@ function InputPage({setInputData}) {
       startingPrice: bid,
       category: cate,
       lifestyle: life,
-      liveTime: moment(startDate).format("YYYY-MM-DDTHH:mm:sszz")
+      liveTime: moment(startDate)?.format("YYYY-MM-DDTHH:mm:sszz")
     }
     setInputData(serialize)
   }, [name, content, price, bid, cate, life, startDate])

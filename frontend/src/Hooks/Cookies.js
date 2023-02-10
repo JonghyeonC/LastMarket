591d2723fec694b3f661b266bfc116b4947fbbc5
JongHyeon Choi <chn9801@gmail.com>
2023-02-10 12:50:19 +0900

    getCookie

--- frontend/src/Hooks/Cookies.js
+++ frontend/src/Hooks/Cookies.js
@@ -3,9 +3,9 @@ import {Cookies} from 'react-cookie'
 const cookies = new Cookies()
 
 export const setCookie = ()=>{
-	return cookies.set(name, value, {...options})
+	return cookies.set()
 }
 
 export const getCookie = ()=>{
-	return cookies.get(name)
+	return cookies.get()
 }

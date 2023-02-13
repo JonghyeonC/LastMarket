fce3bb936180aefc4516580ea288a553d71d5b63
YuhongKim <loyyir1223@naver.com>
2023-02-13 15:14:46 +0900

    profile

--- frontend/src/Router/Profile.js
+++ frontend/src/Router/Profile.js
@@ -1,14 +1,61 @@
-import ProfileComponent from "../Components/ProfileComponent";
+// import ProfileInfo from "../Components/ProfileInfo";
+import ProfileGoodComponent from "../Components/ProfileGoodComponent";
 import LiveChat from "../Chat";
+
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 function Profile() {
 
+  let [nickName, setNickName] = useState('user')
+  let [lifestyles, setLifestyles] = useState('lifestyle')
+  let [addrs, setAddrs] = useState('addr')
+
+  const getInfo = (() => {
+    return (
+      axios({
+        method: 'get',
+        url: `https://i8d206.p.ssafy.io/api/user`
+      })
+      .then((res) => {
+        console.log(res)
+        console.log('여기 성공')
+        // setNickName()
+        // setLifestyles()
+        // setAddrs()
+      })
+      .catch((res) => console.log('여기 실패'))
+    )
+  })
+
+  useEffect(() => {
+    getInfo()
+  }, [])
+
   return (
     <div>
       <div className="profile_Compbox">
-        <div className="profile_Profile_Com">
-          <ProfileComponent />
+        <div className="profile_Profile_Info_Com">
+          <div>
+            <img src="profile_icon.png" width="150px" height="150px" alt="" />
+            <br />
+            <br />
+            <span>
+              <p>닉네임 : {nickName}</p>
+              <p>지역 : {addrs}</p>
+              <p>라이프스타일 : {lifestyles}</p>
+            </span>
+          </div>
+          <br />
+          <div>
+            <button>닉네임 수정</button>
+            <br />
+            <button>프로필 수정</button>
+          </div>
+        </div>
+        <div className="profile_Profile_Good_Com">
+          <ProfileGoodComponent />
         </div>
         <div className="profile_Chat_Com">
           <LiveChat />

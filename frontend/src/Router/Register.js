96e6f82b7d2c100bebcea3123a6cf1ef5f79e801
YuhongKim <loyyir1223@naver.com>
2023-02-13 10:17:09 +0900

    Feat(FE) : register and go home 등록 후 홈으로 리다이렉팅 기능 Related to : #

--- frontend/src/Router/Register.js
+++ frontend/src/Router/Register.js
@@ -1,5 +1,5 @@
 import './Routercss.css'
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import UploadPage from '../Pages/Register/UploadPage'

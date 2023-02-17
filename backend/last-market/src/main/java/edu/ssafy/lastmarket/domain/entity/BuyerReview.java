98f2ff297fc15bfb08fe6aab39ad3937f29d793f
limecats <limecats0331@gmail.com>
2023-02-17 12:47:40 +0900

    fix(BE): add GenerateValue in Review

--- backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/entity/BuyerReview.java
+++ backend/last-market/src/main/java/edu/ssafy/lastmarket/domain/entity/BuyerReview.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class BuyerReview extends BaseEntity{
     @Id
+    @GeneratedValue
     @Column(name = "send_review_id")
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
